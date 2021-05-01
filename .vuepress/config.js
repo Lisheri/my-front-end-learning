4e1ecf06db89ba89afa33bb2cadb807f541c49b2
Lisheri <mohongen@mohongendeMacBook-Pro.local>
2021-05-01 18:13:42 +0800

    迁移博客

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -21,6 +21,13 @@ module.exports = {
   ],
   "theme": "reco",
   "themeConfig": {
+    "mode": 'light', // 默认 auto，auto 跟随系统，dark 暗色模式，light 亮色模式
+    "modePicker": true, // 默认 true，false 不显示模式调节按钮，true 则显示
+    "subSidebar": "auto",
+    "valineConfig": {
+      "appId": 'IitoRlYOaTEqCmJddwQ4lHGT-gzGzoHsz',// your appId
+      "appKey": 'GfAYiCQelHwHdrrxUeXIK3gN', // your appKey
+    },
     "nav": [
       {
         "text": "主页",
@@ -55,11 +62,90 @@ module.exports = {
       }
     ],
     "sidebar": {
-      "/docs/theme-reco/": [
-        "",
-        "theme",
-        "plugin",
-        "api"
+      "/blogs/ECMA2015/": [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "11",
+        "12",
+        "13",
+        "14",
+        "15",
+      ],
+      "/blogs/FunctionalProgramming/": [
+        "FunctionalProgrammingParadigm/121501",
+        "PureFunction/1",
+        "FunctionCombination/1",
+        "Functor/1"
+      ],
+      "/blogs/JSAsyncProgramming/": [
+        "async/092101",
+        "promise/121501"
+      ],
+      "/blogs/performance/": [
+        "1", "2", "3", "4", "5", "6", "7"
+      ],
+      "/blogs/typeEnhancement/": [
+        {
+          title: "JS类型系统",
+          collapsable: false,
+          children: [
+            "1",
+            "2",
+            "3",
+            "4",
+            {
+              title: "TypeScript",
+              collapsable: true,
+              children: [
+                "ts/1",
+                "ts/2",
+                "ts/3",
+                "ts/4",
+                "ts/5",
+                "ts/6",
+                "ts/7"
+              ]
+            }
+          ]
+        }
+      ],
+      "/blogs/part6/": [
+        {
+          title: "前端工程化",
+          collapsable: false,
+          children: [
+            "first/1", 
+            "first/2", 
+            "first/3"
+          ]
+        },
+        {
+          title: "脚手架工具",
+          collapsable: true,
+          children: [
+            "second/1", 
+            "second/2", 
+            "second/3", 
+            "second/4"
+          ]
+        },
+        {
+          title: "自动化构建",
+          collapsable: true,
+          children: [
+            "third/1", 
+            "third/2", 
+            "third/3"
+          ]
+        }
       ]
     },
     "type": "blog",
